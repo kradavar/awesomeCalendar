fea0e5910c6c7cd7fe52cdf24b8292bd9081ca65
Darya Krasava <kradavar@gmail.com>
2020-03-02 13:40:13 +0300

    create events with time

--- src/components/events-component/NewEventButton.jsx
+++ src/components/events-component/NewEventButton.jsx
@@ -1,48 +1,42 @@
-import React, { useState } from "react";
-import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import NewEventModal from "./NewEventModal";
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import NewEventModal from './NewEventModal';
+import colors from '../../constants/theme';
 
 const NewEventButton = () => {
-  const [isModalShown, setModalVisability] = useState(false);
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => setModalVisability(true)}
-        style={styles.buttonWrapper}>
-        <FontAwesomeIcon icon={faPlus} style={styles.plusButton} />
-      </TouchableOpacity>
+	const [isModalShown, setModalVisability] = useState(false);
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity onPress={() => setModalVisability(true)} style={styles.buttonWrapper}>
+				<FontAwesomeIcon icon={faPlus} style={styles.plusButton} />
+			</TouchableOpacity>
 
-      {isModalShown && (
-        <NewEventModal
-          visible={isModalShown}
-          hideModal={() => setModalVisability(false)}
-        />
-      )}
-    </View>
-  );
+			{isModalShown && <NewEventModal visible={isModalShown} hideModal={() => setModalVisability(false)} />}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-    width: "100%",
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'flex-end',
+		width: '100%',
 
-    position: "absolute",
-    bottom: 36,
-    right: 30
-  },
-  plusButton: {
-    color: "white"
-  },
-  buttonWrapper: {
-    borderRadius: 50,
-    backgroundColor: "blue",
-    padding: 10
-  }
+		position: 'absolute',
+		bottom: 36,
+		right: 30,
+	},
+	plusButton: {
+		color: 'white',
+	},
+	buttonWrapper: {
+		borderRadius: 50,
+		backgroundColor: colors.MAIN_COLOR,
+		padding: 10,
+	},
 });
 
 export default NewEventButton;
--- src/components/events-component/NewEventModal.jsx
+++ src/components/events-component/NewEventModal.jsx
@@ -1,22 +1,32 @@
 import React, { useContext, useState } from 'react';
-import { View, Modal, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { View, Modal, Text, TouchableOpacity, StyleSheet, TextInput, CheckBox } from 'react-native';
 import { calendarContext } from '../calendar/context';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { format } from 'date-fns';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import { format, setHours, setMinutes, getHours, getMinutes } from 'date-fns';
 
 const NewEventModal = ({ visible, hideModal }) => {
 	const { currentDate, addEvent } = useContext(calendarContext);
 	const [isShownStart, showStartPicker] = useState(false);
 	const [isShownEnd, showEndPicker] = useState(false);
-	const [startDate, setStartDate] = useState('Choose start date');
-	const [endDate, setEndDate] = useState('Choose end date');
+	const [isAllDayEvent, setEventDuration] = useState(false);
+	const [startDate, setStartDate] = useState(format(currentDate, 'PP'));
+	const [isStartFilled, setStartFilled] = useState(false);
+	const [isEndFilled, setEndFilled] = useState(false);
+	const [endDate, setEndDate] = useState(format(currentDate, 'PP'));
 	const [eventName, setEventName] = useState('');
 
-	const handleStartDateChanging = (date, isStart) => {
+	const handleDateChanging = (date, isStart) => {
+		const setFnc = isStart ? setStartDate : setEndDate;
+		const setFilledFnc = isStart ? setStartFilled : setEndFilled;
+		const formatStr = isAllDayEvent ? 'PP' : 'MMM dd, yyyy, HH:mm';
+		if (!date) {
+			return;
+		}
 		showStartPicker(false);
 		showEndPicker(false);
-		const setFnc = isStart ? setStartDate : setEndDate;
-		setFnc(format(new Date(date), 'PP'));
+
+		setFnc(format(date, formatStr));
+		setFilledFnc(true);
 	};
 
 	const handleEventAdding = () => {
@@ -24,12 +34,11 @@ const NewEventModal = ({ visible, hideModal }) => {
 			name: eventName,
 			startDate,
 			endDate,
+			isAllDayEvent,
 		};
-		console.log(event);
 		addEvent(event);
 		hideModal();
 	};
-
 	return (
 		<View>
 			<Modal
@@ -49,25 +58,37 @@ const NewEventModal = ({ visible, hideModal }) => {
 							<Text>Event name:</Text>
 							<TextInput placeholder="Enter event name" onChangeText={text => setEventName(text)} />
 						</View>
+						<View style={styles.checkBoxWrapper}>
+							<CheckBox onValueChange={() => setEventDuration(!isAllDayEvent)} value={isAllDayEvent} />
+							<Text>All day event</Text>
+						</View>
 						<View style={styles.inputWrapper}>
 							<Text>Start:</Text>
 							<TouchableOpacity onPress={() => showStartPicker(true)}>
-								<Text>{startDate}</Text>
+								<Text>{isStartFilled ? startDate : 'Choose event start'}</Text>
 							</TouchableOpacity>
-							{isShownStart && (
-								<DateTimePicker value={currentDate} onChange={(e, date) => handleStartDateChanging(date, true)} />
-							)}
+							<DateTimePickerModal
+								isVisible={isShownStart}
+								date={new Date(startDate)}
+								onConfirm={date => handleDateChanging(date, true)}
+								onCancel={() => {}}
+								mode={isAllDayEvent ? 'date' : 'datetime'}
+							/>
 						</View>
 						<View style={styles.inputWrapper}>
 							<Text>End:</Text>
 							<TouchableOpacity onPress={() => showEndPicker(true)}>
-								<Text>{endDate}</Text>
+								<Text>{isEndFilled ? endDate : 'Choose event end'}</Text>
 							</TouchableOpacity>
-							{isShownEnd && (
-								<DateTimePicker value={currentDate} onChange={(e, date) => handleStartDateChanging(date, false)} />
-							)}
+							<DateTimePickerModal
+								isVisible={isShownEnd}
+								date={new Date(endDate)}
+								onConfirm={date => handleDateChanging(date, false)}
+								onCancel={() => {}}
+								mode={isAllDayEvent ? 'date' : 'datetime'}
+							/>
 						</View>
-						<TouchableOpacity onPress={handleEventAdding}>
+						<TouchableOpacity onPress={handleEventAdding} disabled={!eventName}>
 							<Text> add new event</Text>
 						</TouchableOpacity>
 					</View>
@@ -101,6 +122,11 @@ const styles = StyleSheet.create({
 	closeButton: {
 		position: 'absolute',
 		right: 5,
+	},
+	checkBoxWrapper: {
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
 	},
 });
 
